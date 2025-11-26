d3c4b0d2bd52f9966aa19251fa06cf428e84a9c0
Víctor Mena Sánchez <vmenasanchez@gmail.com>
2025-11-26 11:04:23 +0100

    application properties updated and main app cleaned

--- src/main/kotlin/com/victor/backendchallenge/BackendchallengeApplication.kt
+++ src/main/kotlin/com/victor/backendchallenge/BackendchallengeApplication.kt
@@ -1,10 +1,6 @@
 package com.victor.backendchallenge
 
-import com.victor.backendchallenge.service.SpotifyService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
@@ -13,16 +9,3 @@ class BackendchallengeApplication
 fun main(args: Array<String>) {
     runApplication<BackendchallengeApplication>(*args)
 }
-
-//fun main(args: Array<String>) {
-  //  println("Hello there!")
-
-    //Guardo el texto
-    //val context = runApplication<BackendchallengeApplication>(*args)
-
-    //Cojo el bean de spotify sevice
-    //val spotifyService = context.getBean(SpotifyService::class.java)
-
-    //Llamo al metodo
-    //spotifyService.printCredentials()
-//}
--- src/main/kotlin/com/victor/backendchallenge/service/PlayListService.kt
+++
@@ -1,8 +0,0 @@
-package com.victor.backendchallenge.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class PlaylistService {
-    fun getFeaturedPlaylists(page: Int, size: Int) = emptyList<Any>()
-}
